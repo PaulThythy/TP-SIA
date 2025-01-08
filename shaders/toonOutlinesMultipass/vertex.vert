7b61a86db905285db215f63448ae4ebfdc40bdcc
PaulThythy <paul.thi@hotmail.fr>
2025-01-09 00:09:37 +0100

    cleaning code

--- shaders/toonOutlinesMultipass/vertex.vert
+++ shaders/toonOutlinesMultipass/vertex.vert
@@ -1,20 +1,5 @@
 #version 450
 
-uniform mat4 MVP;
-uniform mat4 MODEL;
-
-layout(location = 0) in vec3 position;
-layout(location = 2) in vec3 normal;
-
-out vec3 fragPosition;
-out vec3 fragNormal;
-
 void main() {
-    gl_Position = MVP * vec4(position, 1.0);
-
-    vec4 posWorld = MODEL * vec4(position, 1.0);
-    fragPosition = posWorld.xyz;
-
-    mat3 normalMatrix = transpose(inverse(mat3(MODEL)));
-    fragNormal = normalize(normalMatrix * normal);
+    //TODO
 }
--- src/main.cpp
+++ src/main.cpp
@@ -23,30 +23,10 @@
 #include "../glm/glm.hpp"
 #include "../glm/gtc/matrix_transform.hpp"
 
-using namespace glm;
-using namespace std;
-
-#define P_SIZE 3
-#define N_SIZE 3 // c'est forcement 3
-#define C_SIZE 3
-
-#define N_VERTS 8
-#define N_VERTS_BY_FACE 3
-#define N_FACES 12
-
-#define NB_R 40
-#define NB_r 20
-#define BUFFER_OFFSET(i)((char * ) NULL + (i))
-
-GLfloat sommets[(NB_R + 1) * (NB_r + 1) * 3]; // x 3 coordonnées (+1 acr on double les dernierspoints pour avoir des coord de textures <> pour les points de jonctions)
-GLuint indices[NB_R * NB_r * 6]; // x6 car pour chaque face quadrangulaire on a 6 indices (2 triangles=2x 3 indices)
-GLfloat coordTexture[(NB_R + 1) * (NB_r + 1) * 2]; // x 2 car U+V par sommets
-GLfloat normales[(NB_R + 1) * (NB_r + 1) * 3];
+#include "ppm.h"
+#include "tore.h"
 
 // initialisations
-
-void genereVBO();
-void deleteVBO();
 void traceObjet();
 
 // fonctions de rappel de glut
@@ -55,10 +35,7 @@ void clavier(unsigned char, int, int);
 void mouse(int, int, int, int);
 void mouseMotion(int, int);
 void reshape(int, int);
-// misc
-void drawString(const char * str, int x, int y, float color[4], void * font);
-void showInfo();
-void * font = GLUT_BITMAP_8_BY_13; // pour afficher des textes 2D sur l'ecran
+
 // variables globales pour OpenGL
 bool mouseLeftDown;
 bool mouseRightDown;
@@ -72,7 +49,6 @@ float cameraDistance = 0.;
 //--------------------------
 GLuint programID; // handle pour le shader
 GLuint MatrixIDMVP, MatrixIDView, MatrixIDModel, MatrixIDPerspective; // handle pour la matrice MVP
-GLuint VBO_sommets, VBO_normales, VBO_indices, VBO_UVtext, VAO;
 GLuint locCameraPosition;
 GLuint locmaterialShininess;
 GLuint locmaterialSpecularColor;
@@ -82,23 +58,19 @@ GLuint locLightIntensities; //a.k.a the color of the light
 GLuint locLightAttenuation;
 GLuint locLightAmbientCoefficient;
 
-// location des VBO
-//------------------
-GLuint indexVertex = 0, indexUVTexture = 1, indexNormale = 2;
-
 //variable pour paramétrage eclairage
 //--------------------------------------
-vec3 cameraPosition(0., 0., 3.);
+glm::vec3 cameraPosition(0., 0., 3.);
 // le matériau
 //---------------
 GLfloat materialShininess = 3.;
-vec3 materialSpecularColor(1., .1, 1); 
-vec3 materialAlbedo(1, 1, 1);                 // couleur du materiau
+glm::vec3 materialSpecularColor(1., .1, 1); 
+glm::vec3 materialAlbedo(1, 1, 1);                 // couleur du materiau
 
 // la lumière
 //-----------
-vec3 LightPosition(1., 0., .5);
-vec3 LightIntensities(1., 1., 1.); // couleur la lumiere
+glm::vec3 LightPosition(1., 0., .5);
+glm::vec3 LightIntensities(1., 1., 1.); // couleur la lumiere
 GLfloat LightAttenuation = 1.;
 GLfloat LightAmbientCoefficient = .1;
 
@@ -108,123 +80,12 @@ glm::mat4 Model, View, Projection; // Matrices constituant MVP
 int screenHeight = 500;
 int screenWidth = 500;
 
-// pour la texture
-//-------------------
-GLuint image;
-GLuint bufTexture, bufNormalMap;
-GLuint locationTexture, locationNormalMap;
-
 // pour la map d'environnement 
 //-------------------
 GLuint bufSkybox;
 GLuint locSkybox;
 
-//-------------------------
-void createTorus(float R, float r) {
-  float theta, phi;
-  theta = ((float) radians(360.f)) / ((float) NB_R);
-  phi = ((float)(radians(360.f))) / ((float) NB_r);
-
-  float pasU, pasV;
-  pasU = 1. / NB_R;
-  pasV = 1. / NB_r;
-  for (int i = 0; i <= NB_R; i++)
-    for (int j = 0; j <= NB_r; j++) {
-      float a, b, c;
-      sommets[(i * (NB_r + 1) * 3) + (j * 3)] = (R + r * cos((float) j * phi)) * cos((float) i * theta); //x
-      sommets[(i * (NB_r + 1) * 3) + (j * 3) + 1] = (R + r * cos((float) j * phi)) * sin((float) i * theta); //y
-      sommets[(i * (NB_r + 1) * 3) + (j * 3) + 2] = r * sin((float) j * phi);
-
-      normales[(i * (NB_r + 1) * 3) + (j * 3)] = cos((float) j * phi) * cos((float) i * theta); //x
-      normales[(i * (NB_r + 1) * 3) + (j * 3) + 1] = cos((float) j * phi) * sin((float) i * theta); //y
-      normales[(i * (NB_r + 1) * 3) + (j * 3) + 2] = sin((float) j * phi);
-
-      coordTexture[(i * (NB_r + 1) * 2) + (j * 2)] = ((float) i) * pasV;
-      coordTexture[(i * (NB_r + 1) * 2) + (j * 2) + 1] = ((float) j) * pasV;
-    }
-
-  int indiceMaxI = ((NB_R + 1) * (NB_r)) - 1;
-  int indiceMaxJ = (NB_r + 1);
-
-  for (int i = 0; i < NB_R; i++)
-    for (int j = 0; j < NB_r; j++) {
-      int i0, i1, i2, i3, i4, i5;
-      indices[(i * NB_r * 6) + (j * 6)] = (unsigned int)((i * (NB_r + 1)) + j);
-      indices[(i * NB_r * 6) + (j * 6) + 1] = (unsigned int)((i + 1) * (NB_r + 1) + (j));
-      indices[(i * NB_r * 6) + (j * 6) + 2] = (unsigned int)(((i + 1) * (NB_r + 1)) + (j + 1));
-      indices[(i * NB_r * 6) + (j * 6) + 3] = (unsigned int)((i * (NB_r + 1)) + j);
-      indices[(i * NB_r * 6) + (j * 6) + 4] = (unsigned int)(((i + 1) * (NB_r + 1)) + (j + 1));
-      indices[(i * NB_r * 6) + (j * 6) + 5] = (unsigned int)(((i) * (NB_r + 1)) + (j + 1));
-    }
-
-}
-
-//----------------------------------------
-GLubyte * glmReadPPM(char * filename, int * width, int * height)
-//----------------------------------------
-{
-  FILE * fp;
-  int i, w, h, d;
-  unsigned char * image;
-  char head[70]; /* max line <= 70 in PPM (per spec). */
-
-  fp = fopen(filename, "rb");
-  if (!fp) {
-    perror(filename);
-    return NULL;
-  }
-
-  /* grab first two chars of the file and make sure that it has the
-     correct magic cookie for a raw PPM file. */
-  fgets(head, 70, fp);
-  if (strncmp(head, "P6", 2)) {
-    fprintf(stderr, "%s: Not a raw PPM file\n", filename);
-    return NULL;
-  }
-
-  /* grab the three elements in the header (width, height, maxval). */
-  i = 0;
-  while (i < 3) {
-    fgets(head, 70, fp);
-    if (head[0] == '#') /* skip comments. */
-      continue;
-    if (i == 0)
-      i += sscanf(head, "%d %d %d", & w, & h, & d);
-    else if (i == 1)
-      i += sscanf(head, "%d %d", & h, & d);
-    else if (i == 2)
-      i += sscanf(head, "%d", & d);
-  }
-
-  /* grab all the image data in one fell swoop. */
-  image = new unsigned char[w * h * 3];
-  fread(image, sizeof(unsigned char), w * h * 3, fp);
-  fclose(fp);
-
-  * width = w;
-  * height = h;
-  return image;
-}
-
-//----------------------------------------
-void initTexture(GLuint programID)
-//-----------------------------------------
-{
-  int iwidth, iheight;
-  GLubyte * image = NULL;
-
-  image = glmReadPPM("texture/opengl.ppm", &iwidth, &iheight);
-  glGenTextures(1, &bufTexture);
-  glBindTexture(GL_TEXTURE_2D, bufTexture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexImage2D(GL_TEXTURE_2D, 0, 3, iwidth, iheight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-
-  locationTexture = glGetUniformLocation(programID, "textureSampler"); // et il y a la texture elle même  
-  glBindAttribLocation(programID,indexUVTexture,"vertexUV");	// il y a les coord UV  
-}
+Tore myTore;
 
 void initSkybox(GLuint programID) 
 {
@@ -334,11 +195,11 @@ int main(int argc, char ** argv)
   programID = LoadShaders("shaders/toonOutlines/vertex.vert", "shaders/toon/fragment.frag");
   initOpenGL(programID);
 
-  createTorus(1., .3);
+  myTore.createTorus(1., .3);
 
   // construction des VBO a partir des tableaux du cube deja construit
-  genereVBO();
-  initTexture(programID);
+  myTore.genereVBO();
+  myTore.initTexture(programID, "texture/opengl.ppm");
   initSkybox(programID);
 
   /* enregistrement des fonctions de rappel */
@@ -352,64 +213,8 @@ int main(int argc, char ** argv)
   glutMainLoop();
 
   glDeleteProgram(programID);
-  deleteVBO();
+  myTore.deleteVBO();
   return 0;
-}
-
-void genereVBO() {
-
-  if (glIsBuffer(VBO_sommets) == GL_TRUE) glDeleteBuffers(1, &VBO_sommets);
-  glGenBuffers(1, &VBO_sommets);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_sommets);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(sommets), sommets, GL_STATIC_DRAW);
-
-  if (glIsBuffer(VBO_normales) == GL_TRUE) glDeleteBuffers(1, &VBO_normales);
-  glGenBuffers(1, &VBO_normales);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_normales);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(normales), normales, GL_STATIC_DRAW);
-
-  if (glIsBuffer(VBO_indices) == GL_TRUE) glDeleteBuffers(1, &VBO_indices);
-  glGenBuffers(1, &VBO_indices); // ATTENTIOn IBO doit etre un GL_ELEMENT_ARRAY_BUFFER
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_indices);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  if (glIsBuffer(VBO_UVtext) == GL_TRUE) glDeleteBuffers(1, &VBO_UVtext);
-  glGenBuffers(1, &VBO_UVtext);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_UVtext);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(coordTexture), coordTexture, GL_STATIC_DRAW);
-
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO); // ici on bind le VAO , c'est lui qui recupèrera les configurations des VBO glVertexAttribPointer , glEnableVertexAttribArray...
-  glEnableVertexAttribArray(indexVertex);
-  glEnableVertexAttribArray(indexNormale);
-  glEnableVertexAttribArray(indexUVTexture);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_sommets);
-  glVertexAttribPointer(indexVertex, 3, GL_FLOAT, GL_FALSE, 0, (void * ) 0);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_normales);
-  glVertexAttribPointer(indexNormale, 3, GL_FLOAT, GL_FALSE, 0, (void * ) 0);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO_UVtext);
-  glVertexAttribPointer(indexUVTexture, 2, GL_FLOAT, GL_FALSE, 0, (void * ) 0);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_indices);
-
-  // une fois la config terminée   
-  // on désactive le dernier VBO et le VAO pour qu'ils ne soit pas accidentellement modifié 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
-
-}
-//-----------------
-void deleteVBO()
-//-----------------
-{
-  glDeleteBuffers(1, &VBO_sommets);
-  glDeleteBuffers(1, &VBO_normales);
-  glDeleteBuffers(1, &VBO_indices);
-  glDeleteBuffers(1, &VBO_UVtext);
-  glDeleteBuffers(1, &VAO);
 }
 
 /* fonction d'affichage */
@@ -467,8 +272,8 @@ void traceObjet()
 
 
   //pour l'affichage
-  glBindVertexArray(VAO); // on active le VAO
-  glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0); // on appelle la fonction dessin 
+  glBindVertexArray(myTore.VAO); // on active le VAO
+  glDrawElements(GL_TRIANGLES, sizeof(myTore.indices), GL_UNSIGNED_INT, 0); // on appelle la fonction dessin 
   glBindVertexArray(0); // on desactive les VAO
   glUseProgram(0); // et le pg
 
@@ -553,17 +358,17 @@ void clavier(unsigned char touche, int x, int y) {
     break;
   case 'r':
     /* reload le shader */
-    programID = LoadShaders("shaders/toon/vertex.vert", "shaders/toon/fragment.frag");
+    /*programID = LoadShaders("shaders/toon/vertex.vert", "shaders/toon/fragment.frag");
     glUseProgram(programID); 
     initOpenGL(programID);
-    glutPostRedisplay();
+    glutPostRedisplay();*/
     break;
   case 'R':
     /* reload le shader */
-    programID = LoadShaders("shaders/phong/vertex.vert", "shaders/phong/fragment.frag");
+    /*programID = LoadShaders("shaders/phong/vertex.vert", "shaders/phong/fragment.frag");
     glUseProgram(programID); 
     initOpenGL(programID);
-    glutPostRedisplay();
+    glutPostRedisplay();*/
     break;
 
   case 'q':
